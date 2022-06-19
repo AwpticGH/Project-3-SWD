b4d1017c43a772c3bf2554bde67aa4d5a1540af6
Rafi Fajar Sulaiman <95056688+AwpticGH@users.noreply.github.com>
2022-06-19 12:45:21 +0700

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -11,20 +11,63 @@ function focusOnSearchBox() {
 }
 
 var fromContainer = document.getElementsByClassName("container-lists")[0];
-// var toContainer = document.getElementsByClassName("container-lists")[1];
+var toContainer = document.getElementsByClassName("container-lists")[1];
 // var passContainer = document.getElementsByClassName("container-lists")[2];
 
 function displayListsFrom() {
     fromContainer.style.display = "block";
 }
+function displayListsTo() {
+    toContainer.style.display = "block";
+}
+function dislpayListsPass() {
+    passContainer.style.display = "block";
+}
 
-var buttons = fromContainer.getElementsByClassName("btn-cities");
+var btnFromCity = document.querySelectorAll('.btn-cities-from');
+var btnToCity = document.querySelectorAll(".btn-cities-to");
+var departDate = document.getElementById("departure-date");
+var returnDate = document.getElementById("return-date");
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-        document.getElementById("from-city").innerText = buttons[i].innerHTML;
-        console.log("test");
+btnFromCity.forEach(button => {
+    button.addEventListener("click", function() {
+        document.getElementById("from-city").value = button.innerHTML;
+        fromContainer.style.display = "none";
+        if (document.getElementById("to-city").value == "") {
+            displayListsTo();
+        }
+        else {
+            departDate.click;
+        }
     });
-};
+})
+btnToCity.forEach(button => {
+    button.addEventListener("click", function() {
+        document.getElementById("to-city").value = button.innerHTML;
+        toContainer.style.display = "none";
+        if (document.getElementById("from-city").value == "") {
+            displayListsFrom();
+            console.log(document.getElementById("departure-date"))
+        }
+        else {
+            departDate.click;
+            console.log(departDate);
+        }
+    })
+})
 
-console.log(buttons)
+departDate.valueAsDate = new Date();
+// setup for return date
+var date = new Date();
+var day = date.getDate() + 1;
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) {
+    month = "0" + month;
+}
+if (day < 10) {
+    day = "0" + day;
+}
+var tomorrow = year + "-" + month + "-" + day;
+returnDate.value = tomorrow;
